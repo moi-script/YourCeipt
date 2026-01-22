1053baf341769e0c83aee2af5f3b5e0d2f17fe16
lowkeyCodee <rizskplay@gmail.com>
2026-01-22 09:10:26 +0800

    switch to production

--- src/api/getKeys.js
+++ src/api/getKeys.js
@@ -6,6 +6,6 @@ const getBaseApiUrl = (type) => {
     return type === 'local' ? 'http://localhost:3000' : import.meta.env.VITE_URL_BACKEND 
 }
 
-export const BASE_API_URL = getBaseApiUrl('local');  
+export const BASE_API_URL = getBaseApiUrl('production');  
 
 // console.log(getCloudName());
