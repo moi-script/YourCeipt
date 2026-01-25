7dc66b50d7519942fe0be7940ad5465584498d8f
lowkeyCodee <rizskplay@gmail.com>
2026-01-26 07:28:26 +0800

    change to production

--- src/api/getKeys.js
+++ src/api/getKeys.js
@@ -6,7 +6,7 @@ const getBaseApiUrl = (type) => {
     return type === 'local' ? 'http://localhost:3000' : import.meta.env.VITE_URL_BACKEND 
 }
 
-export const BASE_API_URL = getBaseApiUrl('local');  
+export const BASE_API_URL = getBaseApiUrl('production');  
 
 
 export const getAiDefaultModel = () => import.meta.env.VITE_AI_DEFAULT_MODEL;
